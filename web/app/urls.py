b7a1bd30bac0d7d96ae99b32ee867e71a9961aa8
Kenneth Jiang <kenneth.jiang@gmail.com>
2019-02-07 06:55:57 -0800

    Class based view. Not working because it does not seem to support custom actions

--- web/app/urls.py
+++ web/app/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('printers/', login_required(views.PrinterList.as_view())),
-    path('printers/new/', views.new_printer, name='printers_new'),
-    path('printers/<int:id>/', views.edit_printer, name='printers_edit'),
-    path('printers/<int:id>/delete/', views.delete_printer, name='printers_delete'),
-    path('printers/<int:id>/cancel/', views.delete_printer, name='printers_cancel'),
-    path('printers/<int:id>/resume/', views.delete_printer, name='printers_resume'),
-    path('publictimelapses/', views.publictimelapse_list, name='publictimelapse_list'),
+    path('', index, name='index'),
+    path('printers/', login_required(PrinterList.as_view()), name='printers'),
+    path('printers/<str:pk>/', login_required(PrinterDetail.as_view()), name='printer'),
+    # path('printers/new/', views.new_printer, name='printers_new'),
+    # path('printers/<int:id>/', views.edit_printer, name='printers_edit'),
+    # path('printers/<int:id>/delete/', views.delete_printer, name='printers_delete'),
+    # path('printers/<int:id>/cancel/', views.delete_printer, name='printers_cancel'),
+    # path('printers/<int:id>/resume/', views.delete_printer, name='printers_resume'),
+    path('publictimelapses/', publictimelapse_list, name='publictimelapse_list'),
 ]
--- web/app/views.py
+++ web/app/views.py
@@ -4,8 +4,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
-
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import *
 from .forms import *
@@ -17,46 +16,43 @@ def index(request):
 
 class PrinterList(ListView):
     model = Printer
-    template_name = 'printer_list.html'
 
     def get_queryset(self):
         return Printer.objects.filter(user=self.request.user)
 
-@login_required
-def printers(request):
-    printers = Printer.objects.filter(user=request.user)
-    return render(request, 'printer_list.html', {'printers': printers})
+class PrinterDetail(DetailView):
+    form_class = PrinterForm
+    model = Printer
+    template_name = 'app/printer_wizard.html'
 
-@login_required
-def new_printer(request):
-    form = PrinterForm(request.POST or None, request.FILES or None)
-    if request.method == "POST":
+    def get_object(self):
+        if self.kwargs['pk'] == 'new':
+            return None
+        else:
+            return get_object_or_404(Printer, pk=self.kwargs['pk'], user=self.request.user)
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(instance=self.get_object())
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, request.FILES or None, instance=self.get_object())
         if form.is_valid():
-            printer = form.save(commit=False)
-            printer.user = request.user
-            printer.auth_token = hexlify(os.urandom(10)).decode()
-            printer.save()
-            return redirect('/printers/{}/#step-2'.format(printer.id))
+            if kwargs['pk'] == 'new':
+                printer = form.save(commit=False)
+                printer.user = request.user
+                printer.auth_token = hexlify(os.urandom(10)).decode()
+                printer.save()
+                return redirect('/printers/{}/#step-2'.format(printer.id))
+            else:
+                form.save()
+                return render(request, self.template_name, {'form': form})
 
-    return render(request, 'printer_wizard.html', {'form': form})
+        return render(request, self.template_name, {'form': form})
 
-@login_required
-def edit_printer(request, id):
-    instance = get_object_or_404(Printer, id=id, user=request.user)
-    if request.method == "POST":
-        form = PrinterForm(request.POST or None, instance=instance)
-        if form.is_valid():
-            form.save()
-            return render(request, 'printer_wizard.html', {'form': form})
-    else:
-        return render(request, 'printer_wizard.html', {'form': PrinterForm(instance=instance)})
-
-@login_required
-def delete_printer(request, id):
-    instance = get_object_or_404(Printer, id=id)
-    instance.delete()
-    return redirect('/printers/')
-
+    def delete(self, request, *args, **kwargs):
+        self.get_object().delete()
+        return redirect('/printers/')
 
 @login_required
 def cancel_printer(request, id):
